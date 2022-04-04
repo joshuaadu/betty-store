ba7ea96590726b8a174d27c968a5d7799eae3fe8
Joshua Adu-Mensah (Qwaben Josh) <joshuaadu2001@gmail.com>
2022-04-04 21:31:49 +0000

    add image to Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,10 @@
+import img from "../images/grocery-liuba-bilyk--unsplash.jpg";
 const Login = (props) => {
-  return <h1>Log In</h1>;
+  return (
+    <>
+      <h1>Log In</h1>
+      <img src={img} alt="Grocery Store attendant" />
+    </>
+  );
 };
 export default Login;
