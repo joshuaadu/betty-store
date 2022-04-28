184f566bf1d75f5a62d150349c9a2b7f1673074d
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-04-28 23:37:36 +0000

    complete new header design

--- src/pages/Dashboard/Header.js
+++ src/pages/Dashboard/Header.js
@@ -1,44 +1,48 @@
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import LogoutIcon from "@mui/icons-material/Logout";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import styles from "./Header.module.scss";
 import Button from "../../components/UI/Button";
 const Header = ({ notification, username, title }) => {
-  return (
-    <header className={styles.header}>
-      <Button style={{ marginRight: "max(0.5rem, 5%)" }} active={true}>
-        Add Sale
-      </Button>
-      <div
-        className={`settings ${styles["header-icons"]} ${styles["header-tabs"]}`}
-      >
-        <SettingsOutlinedIcon fontSize="large" />
-      </div>
-      <div
-        className={`${styles.notification} ${styles["header-icons"]} ${styles["header-tabs"]}`}
-      >
-        <NotificationsNoneIcon fontSize="large" />
-        {notification > 0 && (
-          <span className={styles["notification-alert"]}>{notification}</span>
-        )}
-      </div>
-      <div className={`${styles["user-info"]} ${styles["header-tabs"]}`}>
-        <div className={styles.avatar}>
-          <img
-            src="https://source.unsplash.com/random/?people"
-            alt="User profile"
-            loading="lazy"
-          />
-        </div>
-        <div className={styles["user-details"]}>
-          <span className={styles.username}>{username}</span>
-          <span className={styles.title}>{title}</span>
-        </div>
-
-        <ArrowDropDownIcon />
-      </div>
-    </header>
-  );
+	return (
+		<header className={styles.header}>
+			<Button style={{ marginRight: "max(0.5rem, 2%)" }} active={true}>
+				Add Sale
+			</Button>
+			<div
+				className={`settings ${styles["header-icons"]} ${styles["header-tabs"]}`}
+			>
+				<SettingsOutlinedIcon />
+			</div>
+			<div
+				className={`${styles.notification} ${styles["header-icons"]} ${styles["header-tabs"]}`}
+			>
+				<NotificationsNoneIcon />
+				{notification > 0 && (
+					<span className={styles["notification-alert"]}>{notification}</span>
+				)}
+			</div>
+			<div className={`${styles["user-info"]} ${styles["header-tabs"]}`}>
+				<div className={styles.avatar}>
+					<img
+						src="https://source.unsplash.com/random/?people"
+						alt="User profile"
+						loading="lazy"
+					/>
+				</div>
+				<div className={styles["user-details"]}>
+					<span className={styles.username}>{username}</span>
+					<span className={styles.title}>{title}</span>
+				</div>
+			</div>
+			<div
+				className={`settings ${styles["header-icons"]} ${styles["header-tabs"]}`}
+			>
+				<LogoutIcon />
+			</div>
+		</header>
+	);
 };
 
 export default Header;
