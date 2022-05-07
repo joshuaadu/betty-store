9bdb6d9ed92249bba0a41f134feb1c548c7e56bf
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-05-07 23:59:10 +0000

    Build Home view for dashboard

--- src/pages/Dashboard/Views/Home.js
+++ src/pages/Dashboard/Views/Home.js
@@ -1,5 +1,12 @@
+import styles from "./Home.module.scss";
 const Home = (props) => {
-	return <div>Home</div>;
+	return (
+		<section className={styles.container}>
+			<div className={styles.header}>
+				<h2>Dashboard</h2>
+			</div>
+		</section>
+	);
 };
 
 export default Home;
