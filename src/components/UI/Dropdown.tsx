a57b70aa608f3cf8e8b66f1329dcb812186b8462
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-08-22 17:54:00 +0000

    click outside dropdown to close

--- src/components/UI/Dropdown.tsx
+++ src/components/UI/Dropdown.tsx
@@ -1,11 +1,23 @@
-import { ReactNode } from "react";
+import { forwardRef, memo, ReactNode } from "react";
 
 interface Props {
 	children?: ReactNode;
 	className?: string | undefined;
+	list: string[];
+	select: (value: string) => void;
 }
-const Dropdown = ({ children, className, ...props }: Props) => {
-	return <div className={className ? className : " "}>{children}</div>;
-};
 
+const Dropdown = ({ children, className, list, select, ...props }: Props) => {
+	return (
+		<div className={className ? className : " "} data-dropdown-menu>
+			<ul>
+				{list.map((item, index) => (
+					<li key={index} data-dropdown-list onClick={() => select(item)}>
+						{item}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
 export default Dropdown;
--- src/components/UI/selectors/ListSelector.tsx
+++ src/components/UI/selectors/ListSelector.tsx
@@ -1,5 +1,5 @@
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import styles from "../../../styles/Selectors.module.scss";
 import Dropdown from "../Dropdown";
@@ -9,24 +9,43 @@ interface Props {
 }
 const ListSelector = ({ label, list }: Props) => {
 	const [openDropdown, setOpenDropdown] = useState(false);
+	const selectRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const eventHandler = (event: Event) => {
+			if (!selectRef.current?.contains(event.target as HTMLElement)) {
+				setOpenDropdown(false);
+				console.log("Close");
+			}
+		};
+		document.body.addEventListener("click", eventHandler);
+
+		return () => {
+			document.body.removeEventListener("click", eventHandler);
+		};
+	}, []);
 
 	const toggleHandler = () => {
 		setOpenDropdown((prev) => !prev);
 	};
+
+	const selectHandler = (value: string) => {
+		// set selected option
+
+		setOpenDropdown(false);
+	};
 	return (
 		<div className={styles.selector}>
-			<div className={styles.header}>
+			<div className={styles.header} ref={selectRef}>
 				<span>{label}</span>
 				<KeyboardArrowDownIcon onClick={toggleHandler} />
 			</div>
 			{openDropdown && (
-				<Dropdown className={styles.dropdown}>
-					<ul>
-						{list.map((item, index) => (
-							<li key={index}>{item}</li>
-						))}
-					</ul>
-				</Dropdown>
+				<Dropdown
+					className={styles.dropdown}
+					select={selectHandler}
+					list={list}
+				/>
 			)}
 		</div>
 	);
