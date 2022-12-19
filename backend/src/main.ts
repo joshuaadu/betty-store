5892554f1a805273f676845ae90f4c3a9b22ceaf
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2022-12-19 09:29:36 +0000

    add Nx monorepo with Nestjs preset

--- backend/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { abortOnError: false }); //Throw an error instead of returning an exit code 1
-  await app.listen(3000);
-}
-bootstrap();
