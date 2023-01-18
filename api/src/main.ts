5f71b263985d2bc9ab9bdc872fa0ecc5b37698a4
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2023-01-18 16:35:29 +0000

    docs: :memo: setup swagger

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { abortOnError: false }); //Throw an error instead of returning an exit code 1
+  const config = new DocumentBuilder()
+    .setTitle('Betty Store API')
+    .setDescription(
+      'The API definitions for Betty Store sales and inventory management app',
+    )
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
