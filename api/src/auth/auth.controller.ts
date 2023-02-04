835d6fdda06a25154c724635e42a3943feb9e04c
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2023-02-04 22:57:00 +0000

    feat(auth): :sparkles: access user data in auth module

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   @Post('sign-in')
-  login() {
-    return { message: 'submitted' };
+  // signin(@Body() authDto: AuthDto) {
+  signin(@Req() req: Request) {
+    console.log(req.body);
+    return { message: 'submitted', credential: '' };
+    // console.log({ data: authDto });
+    // return { message: 'submitted', credential: authDto };
   }
   @Post('change-password')
   changePassword() {
