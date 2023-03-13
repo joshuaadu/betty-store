995315069617f484bfb105d212073768fef893f5
joshuaadu <joshuaadu2001@gmail.com>
2023-03-13 11:33:21 +0000

    feat: :building_construction: add per page layout configuration
    Help from:  https://brockherion.hashnode.dev/creating-per-page-layouts-with-nextjs-typescript-trcp-and-nextauth   https://stackoverflow.com/questions/73942758/how-to-use-next-auth-with-per-page-layouts-in-next-js-with-typescript

--- bettystore/src/pages/_app.tsx
+++ bettystore/src/pages/_app.tsx
@@ -1,20 +1,43 @@
-import { type AppType } from "next/app";
+import type { ReactElement, ReactNode } from "react";
+import type { NextPage } from "next";
+import { type AppType, AppProps } from "next/app";
 import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+//core
+import "primereact/resources/primereact.min.css";
+//icons
+import "primeicons/primeicons.css";
 
-const MyApp: AppType<{ session: Session | null }> = ({
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+// Add generic type
+type AppPropsWithLayout<P> = AppProps<P> & {
+  Component: NextPageWithLayout<P>;
+};
+
+// Pass `{ session: Session; }` type as generic
+function MyApp({
   Component,
-  pageProps: { session, ...pageProps },
-}) => {
+  pageProps,
+}: AppPropsWithLayout<{ session: Session }>) {
+  const getLayout = Component.getLayout || ((page: any) => page);
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
+    <SessionProvider session={pageProps.session}>
+      {getLayout(
+        <>
+          <Component {...pageProps} />
+        </>
+      )}
     </SessionProvider>
   );
-};
+}
 
 export default api.withTRPC(MyApp);
