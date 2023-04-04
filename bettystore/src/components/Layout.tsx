488e82aeca3af4c649ece22185b9599ac217f34f
joshuaadu <joshuaadu2001@gmail.com>
2023-04-04 17:06:45 +0000

    fix import errors

--- bettystore/src/components/Layout.tsx
+++ bettystore/src/components/Layout.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import Header from "./Header";
+import SideBarToggle from "./SideBarToggle";
+import SideBar from "./SideBar";
+import Content from "./Content";
 
 export default function Layout() {
   return (
